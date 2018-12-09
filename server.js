9f7df054a3ab59658d6ac3aa1ba861f69d449e7c
Shubham Tunwal <shubhamtunwal97@gmail.com>
2018-12-09 19:09:32 +0530

    fixed server.js port -> Port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express=require("express");
 var app=express();
-var port=process.env.port || 8080;
+var port=process.env.Port || 8080;
 app.use(express.static(__dirname));
 
 // Set view engine as EJS
